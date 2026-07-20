33db41a0c7335feba8dba6e37d59beb8ac550359
Viktor Gabriel <gabryoliver22@gmail.com>
2026-07-20 19:48:55 -0300

    feat: criacao do servidor HTTP da aplicacao

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("Hello World")
+import * as http from 'http'
+
+const server =  http.createServer((req:http.IncomingMessage,res:http.ServerResponse) => {
+    console.log(req)
+});
+
+server.listen(process.env.PORT, () => {
+    console.log(`Servidor inicio na porta ${process.env.PORT}`)
+})
+
